6f5d10c081ddc3c5e5df7528b7c93c92e20c844e
Catherine Smith <catsmith24601@gmail.com>
2021-06-22 13:45:58 -0400

    set up endpoints for api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,17 @@
+
+// use aggregate function to dynamically add up and return the total duration for each workout
+// endpoints defined in api.js file in public folder
+
+// Get (read) workouts
+'/api/workouts'
+
+'api/workouts/range'
+
+// Post (create/insert) workouts
+'/api/workouts'
+
+// Put (update) workouts
+'api/workouts/:id'
+
+
+// Delete (remove) workouts
