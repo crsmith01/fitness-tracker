c78726b045af78a8be3eeaf34002f876b1ecaafe
Catherine Smith <catsmith24601@gmail.com>
2021-06-22 12:57:09 -0400

    added server

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Port
+const PORT = process.env.PORT || 3002;
+
+// Require the models folder for the database
+const db = require("./models");
+
+const app = express();
+
+// Middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Connect to Mongoose database and logging port
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+
+// Routes
+
+
+// Calling server to listen
+app.listen(3002, () => {
+    console.log("App running on port 3002!");
+});
